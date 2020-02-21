a27c3e9769f496ca572b9c32cee68fb954c85827
Lucas De Marchi <lucas.de.marchi@gmail.com>
2020-02-21 02:01:01 -0800

    Add install.sh to skycontroller2
    Just a bunch of adb commands to copy everything.

---
+++ distro/skycontroller2/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+adb shell mkdir -p /data/ftp/internal_000/ardupilot/bin/
+adb push ./data/ftp/internal_000/ardupilot/bin/demarc-btn /data/ftp/internal_000/ardupilot/bin/
+adb push ./data/ftp/internal_000/ardupilot/bin/demarc-cm /data/ftp/internal_000/ardupilot/bin/
+adb shell chmod +x /data/ftp/internal_000/ardupilot/bin/demarc-btn
+adb shell chmod +x /data/ftp/internal_000/ardupilot/bin/demarc-cm
+
+adb shell mount -o remount,rw /
+
+adb shell mkdir -p /etc/dema-rc/
+adb push ./etc/dema-rc/dema-rc.conf /etc/dema-rc/
+
+adb push ./etc/boxinit.d/99-demarc.rc /etc/boxinit.d/
+adb shell chmod 0400 /etc/boxinit.d/99-demarc.rc
