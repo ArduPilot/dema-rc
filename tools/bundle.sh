a00344b7c3b8dbaf15de98a09e535d65dede7863
Lucas De Marchi <lucas.de.marchi@gmail.com>
2020-06-19 10:36:49 -0700

    build: overlay files according to configured board
    This allows to easily create the bundle for a configured board. Assuming
    default configuration for sc2:
    
    	# configuration
    	meson setup -Dboard=sc2 \
    		--cross-file /opt/arm-buildroot-linux-gnueabihf_sdk-buildroot/etc/meson/cross-compilation.conf build
    
    	# build
    	ninja -C build
    
    	# bundle
    	ninja -C build bundle
    
    This will create a tarball build/dema-rc-bundle.tar.gz containing
    everything for that board, including any file relocations needed.

--- tools/bundle.sh
+++ tools/bundle.sh
@@ -7,9 +7,31 @@ SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 BUILD_DIR=${BUILD_DIR:-$(realpath $SCRIPT_DIR/../build)}
 TOOLCHAIN_DIR=${TOOLCHAIN_DIR:-/opt/arm-buildroot-linux-gnueabihf_sdk-buildroot}
 TOOLCHAIN=${TOOLCHAIN:-arm-buildroot-linux-gnueabihf}
-OUTPUT=${1:-dema-rc-bundle.tar.gz}
+
+BOARD=
+BOARD_OPTIONS=
+OUTPUT=
+
+args=0
+while getopts "o:b:" o; do
+    case "${o}" in
+        o) OUTPUT=${OPTARG}; args=$[$args + 2] ;;
+	b) BOARD=${OPTARG}; args=$[$args + 2] ;;
+        \?) exit 1;;
+    esac
+done
+shift $args
+
+OUTPUT=${OUTPUT:-dema-rc-bundle.tar.gz}
 
 instdir=
+
+if [ "$BOARD" == "sc2" ]; then
+	BOARD_OPTIONS="-x ld-*.so -x libc.so -q qemu-arm-static"
+else
+	echo "Unknown distro option $BOARD" >/dev/stderr
+	exit 1
+fi
 
 trap '
     ret=$?;
@@ -23,12 +45,13 @@ trap '
     exit $ret;
     ' EXIT
 
+
 relocate_bundle() {
 	local PREFIX=data/ftp/internal_000/dema-rc/usr
 	local LIBDIR="$PREFIX/lib"
 	local BINDIR="$PREFIX/bin"
 
-	pushd $instdir
+	pushd $instdir > /dev/null
 	mkdir -p $PREFIX
 	if [ -d usr/lib ]; then
 		mv usr/lib $PREFIX/
@@ -45,28 +68,29 @@ relocate_bundle() {
 	if [ -x ${PREFIX}${ld} ]; then
 		patchelf --set-interpreter /${PREFIX}${ld} "$BINARY"
 	fi
-	popd
+	popd > /dev/null
 }
 
 # clean up after ourselves no matter how we die.
 trap 'exit 1;' SIGINT
 
 instdir=$(mktemp -d --tmpdir dema-rc-bundle.XXXXXXXX)
-DESTDIR="$instdir" ninja -C $SCRIPT_DIR/../build install
+DESTDIR="$instdir" ninja -C $SCRIPT_DIR/../build install > /dev/null
 
 $SCRIPT_DIR/bundler.sh \
-	-x 'ld-*.so' -x libc.so \
-	-q qemu-arm-static \
+	$BOARD_OPTIONS \
 	-d -o $instdir \
 	"$TOOLCHAIN_DIR" \
 	"$TOOLCHAIN" \
 	"$instdir/usr/bin/dema-rc"
 
-# overlay distro/sc2/
-cp -a $SCRIPT_DIR/../distro/sc2/. "$instdir"
+# overlay distro/$BOARD/
+cp -a $SCRIPT_DIR/../distro/$BOARD/. "$instdir"
 
 # relocate binaries and libs to side load
 relocate_bundle
 
-rm -f dema-rc-bundle.tar.gz
+rm -f $OUTPUT
 tar --owner=0 --group=0 -C "$instdir" -czf ${OUTPUT}  .
+
+echo "Bundle created: $(realpath $OUTPUT)"
